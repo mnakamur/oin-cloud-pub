55ce8340f1dff613a28b8a46843162ce295be0f3
MASA-PC\fwba0_000 <mnakamur@market-interface.co.jp>
2025-02-20 15:13:06 +0900

    oin-cloud move to asset

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
       </div>
       <ul class="footElemnt">
         <li>
-          <a href="guide/oin-cloudDoc.html" target="_blank" rel="noopener">押印クラウドとは</a>
+          <a href="../assets/oin-cloudDoc.html" target="_blank" rel="noopener">押印クラウドとは</a>
         </li>
         <li>
           <a
