1780a5e33caa7b43517ef6fd9df933a2bc5e5383
MASA-PC\fwba0_000 <mnakamur@market-interface.co.jp>
2025-03-06 16:42:47 +0900

    shomepdfsign docname console

--- amplify/backend/function/shomeiPdfSign/src/index.js
+++ amplify/backend/function/shomeiPdfSign/src/index.js
@@ -50,9 +50,10 @@ exports.handler = async (event) => {
   }
   const eventBody = event.body;
   //const eventBody = parseRequestBody(event.body);
-  console.log('eventBody=', eventBody);
+
   const pathName = 'protected' + '/' + eventBody.s3folder + '/' + eventBody.createUserId;
   const docfileName = pathName + '/' + eventBody.pdfId;
+  console.log('docfileName=', docfileName, bucket);
   let pdfBuffer = await s3GetObject(docfileName);
   let dumx509 = process.env.DUM_X509;
   pemCert = await s3GetObject(dumx509);
